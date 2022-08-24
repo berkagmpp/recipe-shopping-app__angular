2c24542ff478de953a67b4391595f1f4cb404a2b
berkagmpp <berkagmpp@gmail.com>
2022-08-24 15:38:19 +1200

    feat: Add basic components and connect to appComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
