a064ecc0d6fa8891b4048be777d7b5a74c3a783d
berkagmpp <berkagmpp@gmail.com>
2022-08-25 14:07:01 +1200

    feat: Add other test recipe in recipeListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.module';
 })
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simple test recipe', 'https://www.health.com/thmb/HYCUnztcpvlKSASwjJP6GUDHPqI=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():format(webp)/Tout-Pizza-Recipes-Gallery-2000-cfbe06b50c2a4542976cd33378ab7acd.jpg') 
+        new Recipe('A Test Recipe', 'This is simple test recipe', 'https://www.health.com/thmb/HYCUnztcpvlKSASwjJP6GUDHPqI=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():format(webp)/Tout-Pizza-Recipes-Gallery-2000-cfbe06b50c2a4542976cd33378ab7acd.jpg'),
+        new Recipe('A Test Recipe 2', 'This is simple test recipe 2', 'https://www.health.com/thmb/HYCUnztcpvlKSASwjJP6GUDHPqI=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():format(webp)/Tout-Pizza-Recipes-Gallery-2000-cfbe06b50c2a4542976cd33378ab7acd.jpg') 
     ];
 
     constructor() { }
