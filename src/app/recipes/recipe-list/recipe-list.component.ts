d91ae41129514a8979262a23ffca47cffd0a99ae
berkagmpp <berkagmpp@gmail.com>
2022-08-25 13:56:24 +1200

    feat: Add Recipe Model class and adapt to recipeListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.module';
+
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simple test recipe', 'https://www.health.com/thmb/HYCUnztcpvlKSASwjJP6GUDHPqI=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():format(webp)/Tout-Pizza-Recipes-Gallery-2000-cfbe06b50c2a4542976cd33378ab7acd.jpg') 
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
