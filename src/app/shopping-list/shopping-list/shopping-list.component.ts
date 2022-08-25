fc3f9274868f0560069a4e5c07ea257a7a97c803
berkagmpp <berkagmpp@gmail.com>
2022-08-25 22:01:43 +1200

    feat: Add Ingredient Model class in /shared and adapt to ShoppingListComponent

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 3),
+        new Ingredient('Tomatoes', 5),
+        new Ingredient('Bananas', 2)
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
